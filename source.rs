5ac8c552c88b62093032c3e76c860a030b0f8b78
shominalexander <40673785+shominalexander@users.noreply.github.com>
2023-06-19 10:12:20 +0300

    Update source.rs

--- source.rs
+++ source.rs
@@ -1,13 +1,13 @@
 use std::env;                      // let args: Vec<String> = env::args().collect();
 use std::fs;                       // for item in fs::read_dir(folder).unwrap() {
-use std::io::{BufRead, BufReader}; // let     lines = BufReader::new(file).lines();
+use std::io::{BufRead, BufReader}; // let     items   = BufReader::new(fs::File::open(&name).unwrap()).lines();
 use std::io::Write;                // file.write( text.to_string() );
 use std::{ffi::OsStr, path::Path}; // if Path::new(name).extension().and_then(OsStr::to_str) == Some(extension) {
 
-fn level(extension: &String, folder: &String, new_1: &String, new_2: &String, new_3: &String, old: &String, skip: usize, target: &String, whole: &String) {
+fn level(extension: &String, folder: &String, limit: usize, new_1: &String, new_2: &String, new_3: &String, old: &String, skip: usize, target: &String, whole: &String) {
  for item in fs::read_dir(folder).unwrap() {
   if item.as_ref().unwrap().path().is_dir() {
-   level(&extension, &item.as_ref().unwrap().path().display().to_string(), &new_1, &new_2, &new_3, &old, skip, &target, &whole);
+   level(&extension, &item.as_ref().unwrap().path().display().to_string(), limit, &new_1, &new_2, &new_3, &old, skip, &target, &whole);
 
   } else { //if item.as_ref().unwrap().path().is_dir() {
    let     name = item.as_ref().unwrap().path().display().to_string();
@@ -18,14 +18,14 @@ fn level(extension: &String, folder: &String, new_1: &String, new_2: &String, ne
     if name == path { 
      same = true; 
 
-    } //if name == path { 
+    }//if name == path { 
 
-   } else { //if !target.trim().is_empty() { 
+   } else {//if !target.trim().is_empty() { 
     if Path::new(&name).extension().and_then(OsStr::to_str) == Some(extension) { 
      same = true; 
 
-    } //if Path::new(name).extension().and_then(OsStr::to_str) == Some(extension) { 
-   } //} else { //if !target.trim().is_empty() { 
+    }//if Path::new(name).extension().and_then(OsStr::to_str) == Some(extension) { 
+   }//} else {//if !target.trim().is_empty() { 
 
    if same {
     let mut empty   = 0;
@@ -45,19 +45,19 @@ fn level(extension: &String, folder: &String, new_1: &String, new_2: &String, ne
 
       if skip > 0 { if skipped < skip { found = found - 1; skipped = skipped + 1; } }
 
-      if found > 0 && !new_1.trim().is_empty() {
+      if found > 0 && ( found <= limit || limit == 0 ) && !new_1.trim().is_empty() {
        if !whole.trim().is_empty() {
         match found { 1 =>                                                                  line = new_1.to_string()
                     , 2 => if !new_2.trim().is_empty() { line = new_2.to_string(); } else { line = new_1.to_string(); }
                     , 3 => if !new_3.trim().is_empty() { line = new_3.to_string(); } else { line = new_1.to_string(); }
                     , _ => ()
                     }
-       } else { //if !whole.trim().is_empty() {
+       } else {//if !whole.trim().is_empty() {
         line = line.replace(&old.to_string(), &new_1.to_string());
 
-       } //} else { //if !whole.trim().is_empty() {
-      } //if found > 0 && !new_1.trim().is_empty() {
-     } //if line.find(&old.to_string()) != None {
+       }//} else {//if !whole.trim().is_empty() {
+      }//if found > 0 && ( found <= limit || limit == 0 ) && !new_1.trim().is_empty() {
+     }//if line.find(&old.to_string()) != None {
 
      index = index + 1;
 
@@ -65,12 +65,12 @@ fn level(extension: &String, folder: &String, new_1: &String, new_2: &String, ne
       if index > 1 { 
        text = text + &"\n".to_string() + &line; 
 
-      } else { //if index > 1 { 
+      } else {//if index > 1 { 
        text = line; 
 
-      } //} else { //if index > 1 { 
-     } //if empty < 2 { 
-    } //for item in items {
+      }//} else {//if index > 1 { 
+     }//if empty < 2 { 
+    }//for item in items {
 
     text = text + &"\n".to_string(); 
 
@@ -80,31 +80,33 @@ fn level(extension: &String, folder: &String, new_1: &String, new_2: &String, ne
 
       println!("{:?} bytes written to {}", fs::OpenOptions::new().truncate(true).write(true).open(&name).unwrap().write(&text.as_bytes()).unwrap(), &name);
 
-     } else { //if !new_1.trim().is_empty() {
+     } else {//if !new_1.trim().is_empty() {
       println!("found in {}", name);
 
-     } //} else { //if !new_1.trim().is_empty() {
-    } //if found > 0 {
-   } //if same {
-  } //} else { //if item.as_ref().unwrap().path().is_dir() {
- } //for item in fs::read_dir(folder).unwrap() {
-} //fn level(extension: &String, folder: &String, new_1: &String, new_2: &String, new_3: &String, old: &String, skip: usize, target: &String, whole: &String) {
+     }//} else {//if !new_1.trim().is_empty() {
+    }//if found > 0 {
+   }//if same {
+  }//} else {//if item.as_ref().unwrap().path().is_dir() {
+ }//for item in fs::read_dir(folder).unwrap() {
+}//fn level(extension: &String, folder: &String, limit: usize, new_1: &String, new_2: &String, new_3: &String, old: &String, skip: usize, target: &String, whole: &String) {
 
 fn main() {
- let args: Vec<String> = env::args().collect();
+ let args     : Vec<String> = env::args().collect();
+ let extension:     &String    =     &args[01]        ;
+ let folder   :     &String    =     &args[02]        ;
+ let limit    :     &mut usize =     &mut 0           ;
+ let new_1    :     &String    =     &args[04]        ;
+ let new_2    :     &String    =     &args[05]        ;
+ let new_3    :     &String    =     &args[06]        ;
+ let old      :     &String    =     &args[07]        ;
+ let skip     :     &mut usize =     &mut 0           ;
+ let target   :     &String    =     &args[09]        ;
+ let whole    :     &String    =     &args[10]        ;
 
- let extension = &args[1];
- let folder    = &args[2];
- let new_1     = &args[3];
- let new_2     = &args[4];
- let new_3     = &args[5];
- let old       = &args[6];
- let target    = &args[8];
- let whole     = &args[9];
+ match &args[3].parse::<usize>() { Ok(usize) => { *limit = *usize } Err(error) => { println!("error: {:?}", error); } }
+ match &args[8].parse::<usize>() { Ok(usize) => { *skip  = *usize } Err(error) => { println!("error: {:?}", error); } }
 
- println!("new_1: {}", &new_1);
+ println!("limit: {:?}", limit);
 
- match &args[7].parse::<usize>() { Ok(skip) => level(&extension, &folder, &new_1, &new_2, &new_3, &old,  *skip, &target, &whole)
-                                 , Err(_)   => level(&extension, &folder, &new_1, &new_2, &new_3, &old, 0usize, &target, &whole)
- } //match &args[7].parse::<usize>() {
-} //fn main() {
+ level(&extension, &folder, *limit, &new_1, &new_2, &new_3, &old, *skip, &target, &whole)
+}//fn main() {
